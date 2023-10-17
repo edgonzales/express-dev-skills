f426784691c92bde5f20bd55b550ac631b85d337
Ed Gonzales <edgonzales6@gmail.com>
2023-10-17 15:35:21 -0700

    Lab part 1 complete

--- controllers/skills.js
+++ controllers/skills.js
@@ -7,7 +7,7 @@ module.exports = {
 }
 
 function index(req, res) {
-    res.render('skills/index', {
+    res.render('skills', { // 'skills' is found in views/skills.ejs | if it was under a folder titled 'skills' then it would be 'skills/skills' 
         skills: Skill.getAll()
     });
 }
--- routes/skills.js
+++ routes/skills.js
@@ -1,9 +1,11 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router(); //'Router() is a factory function
+var skillsCtrl = require('../controllers/skills');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// All actual paths start with '/skills'
+
+// GET /todos
+router.get('/', skillsCtrl.index);
+
 
 module.exports = router;
